599164b0b4b2849d3033289f186514dd79efc0a2
OscarTalero <talero@gmail.com>
2022-07-10 22:06:52 -0500

    Fix ManyToOne Movie-Gender1

--- src/main/java/com/alkemy/disney/DTO/GenderIdDTO.java
+++
@@ -1,9 +0,0 @@
-package com.alkemy.disney.DTO;
-
-import lombok.Data;
-
-@Data
-public class GenderIdDTO {
-
-    private Long id;
-}
--- src/main/java/com/alkemy/disney/DTO/GenderNameDTO.java
+++
@@ -1,9 +0,0 @@
-package com.alkemy.disney.DTO;
-
-import lombok.Data;
-
-@Data
-public class GenderNameDTO {
-
-    private String name;
-}
